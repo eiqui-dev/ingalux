33c3bf724c056170faaaa3c47cbb8b775618e9d8
Eiqui <info@eiqui.com>
2018-01-23 12:01:10 +0000

    ADD calc description and price purchase variant

--- aloxa_ingalux/models/inherit_purchase_order_line.py
+++ aloxa_ingalux/models/inherit_purchase_order_line.py
@@ -37,9 +37,22 @@ class purchase_order_line(osv.osv):
             partner_id, date_order=False, fiscal_position_id=False, date_planned=False,
             name=False, price_unit=False, state='draft', context=None):
 	product = self.pool.get('product.product').browse(cr,uid,product_id,context=context)
+	partner = self.pool.get('res.partner').browse(cr,uid,partner_id,context=context)
 	res = super(purchase_order_line, self).onchange_product_id(cr, uid, ids, pricelist_id, product_id, qty, uom_id,
 		partner_id, date_order=False, fiscal_position_id=False, date_planned=False,
                 name=False, price_unit=False, state='draft', context=context)
+	seller_price = context.get('price_unit')
+	for seller in product.variant_seller_ids:
+                        if seller.name.id == partner.id:
+				qty_top = 0
+				for pricelist_item in seller.pricelist_ids:
+					_logger.info(pricelist_item)
+					if qty >= pricelist_item.min_quantity and pricelist_item.min_quantity >= qty_top:
+						qty_top = pricelist_item.min_quantity
+						seller_price = pricelist_item.price
+						_logger.info(qty_top)
+   						_logger.info('A')
+						res['value']['price_unit'] = seller_price
 	if product and product.price_on_uom:
 		if context.get('uom_qty') or context.get('price_by_uom'):
 			if context.get('product_qty'): # If change the product qty we must recalcule the price based on pricelist 
@@ -57,11 +70,17 @@ class purchase_order_line(osv.osv):
 		res['value'].update({
 			'price_on_uom' : product.price_on_uom,
 			'price_unit' : price_uom_calc,
+			'price_by_uom' : price_by_uom,
 		})
 	if product:
                 name = product.with_context(display_default_code=False).display_name
                 if product.description_purchase:
                         name += '\n' + product.description_purchase
+		for seller in product.variant_seller_ids:
+			_logger.info(seller.name)
+                        if seller.name.id == partner.id and seller.product_name:
+				_logger.info("DENTRO")
+                                name = seller.product_name
                 res['value'].update({'name': name})
 	if product and not product.price_on_uom:
 		res['value'].update({
